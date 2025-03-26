bb633f368b2629c6a6c6e646afaec3376001367b
Daniela Castro <dcastrog1@eafit.edu.co>
2025-03-25 19:27:51 -0500

    add Tailwind CSS setup, create Report component, and implement password modal in navigation menu

--- my-remix-app/app/root.tsx
+++ my-remix-app/app/root.tsx
@@ -6,7 +6,7 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
-import "./tailwind.css";
+//import "./tailwind.css";
 
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
